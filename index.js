fcad6fa0a41068deea0a51a14d0c4a979ef3cbd5
Mikko Haapoja <me@mikkoh.com>
2014-10-17 17:21:28 -0400

    Wrote some documentation

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+/** @module google-maps-api */
+
 var script = require( 'scriptjs' ),
 	promise = require( 'promise' );
 
 var maps = null;
 
+/** 
+ * Load a Google Maps API Object asynchronously. This module will return a Promise.
+ * Which will on resolved will return the "google.maps" object.
+ *
+ * Or if you prefer you can simply use the callback instead.
+ * 
+ * @param  {String} apikey Your Google Maps API Key
+ * @param  {Function} [onComplete] A callback which will return the google.maps object
+ * @return {Promise} When this promise resolves it will return the google.maps object
+ *
+ * @example using via promise
+ *
+ * ```javascript
+ * var mapsapi = require( 'google-maps-api' )( 'your api key' );
+ *
+ * mapsapi().then( function( maps ) {
+ *
+ * 	//use the google.maps object as you please
+ * });
+ * ```
+ *
+ * @example using via callback
+ * require( 'google-maps-api' )( 'your api key', function( maps ) {
+ *
+ * 	//use the google.maps object as you please
+ * })
+ */
 module.exports = function( apikey, onComplete ) {
 
 	return function() {
